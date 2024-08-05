993d1e1aa5be7835777cab0d0753e02064f5f2be
Bryant Dawson Priyantoro <briyantazazel7@gmail.com>
2024-08-05 08:11:24 +0800

    Fix ESLint error and many component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,83 +1,100 @@
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import { Input, Tooltip, Typography, Avatar } from "@material-tailwind/react";
-
-import useInput from "../hooks/useInput";
-
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-  MagnifyingGlassIcon,
+  Tooltip,
+  Typography,
+  Avatar,
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from '@material-tailwind/react';
+import {
   ChatBubbleLeftRightIcon,
   ChartBarIcon,
   PlusIcon,
-  ArrowRightStartOnRectangleIcon,
-} from "@heroicons/react/20/solid";
-import { useSelector } from "react-redux";
+} from '@heroicons/react/20/solid';
 
-export default function Header({ onSignOut }) {
+import { asyncUnsetAuthUser } from '../states/authUser/action';
+
+export default function Header() {
   const authUser = useSelector((state) => state.authUser);
+  const dispatch = useDispatch();
 
-  const [keyword, onKeywordChange] = useInput("");
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
+  const onSignOutHandler = () => {
+    dispatch(asyncUnsetAuthUser());
+  };
+
+  if (!authUser) {
+    return (
+      <header className="bg-blue-gray-900 fixed w-full p-4 z-20">
+        <nav className="w-full lg:container lg:mx-auto flex flex-col md:flex-row items-center gap-6 justify-center">
+          <Link to="/" className="flex items-center gap-2 text-white">
+            <ChatBubbleLeftRightIcon className="size-10" />
+            <h1 className="font-mono text-4xl text-white">Discom</h1>
+          </Link>
+        </nav>
+      </header>
+    );
+  }
 
   return (
-    <header className="bg-blue-gray-900 fixed w-full p-4 z-20">
-      <nav className="w-full lg:container lg:mx-auto flex flex-col md:flex-row items-center gap-6 justify-between">
-        <Link to="/" className="flex items-center gap-2 text-white">
-          <ChatBubbleLeftRightIcon className="size-10" />
-          <h1 className="font-mono text-4xl text-white">Discom</h1>
-        </Link>
-        <div className="w-full max-w-[36rem]">
-          <Input
-            value={keyword}
-            onChange={onKeywordChange}
-            color="orange"
-            className="text-orange-400"
-            label="Search thread"
-            icon={<MagnifyingGlassIcon color={keyword ? "orange" : "white"} />}
-          />
-        </div>
-        <ul className="text-white flex gap-0 md:gap-6">
-          <li className="p-3 hover:bg-orange-400 hover:rounded-full hover:transition-all hover:duration-75">
-            <Tooltip
-              content={
-                <Typography color="white" className="text-sm">
-                  Leaderboards
-                </Typography>
-              }
-              placement="bottom"
-              offset={30}
-            >
-              <Link to="/leaderboards">
-                <ChartBarIcon width={30} />
-              </Link>
-            </Tooltip>
-          </li>
-          <li className="p-3 hover:bg-orange-400 hover:rounded-full hover:transition-all hover:duration-75">
-            <Tooltip
-              content={
-                <Typography color="white" className="text-sm">
-                  Create Threads
-                </Typography>
-              }
-              placement="bottom"
-              offset={30}
-            >
-              <Link to="/new">
-                <PlusIcon width={30} />
-              </Link>
-            </Tooltip>
-          </li>
-          <li className="p-2 hover:bg-orange-400 hover:rounded-full hover:transition-all hover:duration-75">
-            {authUser ? (
+    <>
+      <header className="bg-blue-gray-900 fixed w-full p-4 z-20">
+        <nav className="w-full lg:container lg:mx-auto flex flex-col md:flex-row items-center gap-6 justify-between">
+          <Link to="/" className="flex items-center gap-2 text-white">
+            <ChatBubbleLeftRightIcon className="size-10" />
+            <h1 className="font-mono text-4xl text-white">Discom</h1>
+          </Link>
+
+          <ul className="text-white flex gap-0 md:gap-6">
+            <li className="p-3 hover:bg-orange-400 hover:rounded-full hover:transition-all hover:duration-75">
               <Tooltip
-                content={
+                content={(
                   <Typography color="white" className="text-sm">
-                    Logout
+                    Klasemen
                   </Typography>
-                }
+                )}
                 placement="bottom"
                 offset={30}
               >
-                <Link to="/" onClick={onSignOut}>
+                <Link to="/leaderboards">
+                  <ChartBarIcon width={30} />
+                </Link>
+              </Tooltip>
+            </li>
+            <li className="p-3 hover:bg-orange-400 hover:rounded-full hover:transition-all hover:duration-75">
+              <Tooltip
+                content={(
+                  <Typography color="white" className="text-sm">
+                    Buat Diskusi
+                  </Typography>
+                )}
+                placement="bottom"
+                offset={30}
+              >
+                <Link to="/new">
+                  <PlusIcon width={30} />
+                </Link>
+              </Tooltip>
+            </li>
+            <li className="p-2 hover:bg-orange-400 hover:rounded-full hover:transition-all hover:duration-75">
+              <Tooltip
+                content={(
+                  <Typography color="white" className="text-sm">
+                    Keluar
+                  </Typography>
+                )}
+                placement="bottom"
+                offset={30}
+              >
+                <Link to="/" onClick={handleOpen}>
                   <Avatar
                     src={`${authUser.avatar}`}
                     alt={`${authUser.name}`}
@@ -85,28 +102,31 @@ export default function Header({ onSignOut }) {
                   />
                 </Link>
               </Tooltip>
-            ) : (
-              <Tooltip
-                content={
-                  <Typography color="white" className="text-sm">
-                    Login
-                  </Typography>
-                }
-                placement="bottom"
-                offset={30}
-              >
-                <Link to="/">
-                  <ArrowRightStartOnRectangleIcon width={30} />
-                </Link>
-              </Tooltip>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </header>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <Dialog open={open} handler={handleOpen} size="sm">
+        <DialogHeader className="text-center">Logout dari Discom</DialogHeader>
+        <DialogBody>
+          Apakah anda yakin ingin keluar sekarang ? Masih banyak diskusi yang
+          menarik untuk anda 😊
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpen}
+            className="mr-1"
+          >
+            <span>Batal</span>
+          </Button>
+          <Button variant="gradient" color="orange" onClick={onSignOutHandler}>
+            <span>Konfirm</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
   );
 }
-
-Header.propTypes = {
-  onSignOut: PropTypes.func,
-};
